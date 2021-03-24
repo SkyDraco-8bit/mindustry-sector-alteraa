7044255a5f6d104c20d310cdf1ea4abf0a41e531
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-24 12:24:16 +0100

    Create UnitType.java

---
+++ src/sonnicon/modtemplate/UnitType.java
@@ -0,0 +1,41 @@
+ackage mindustry.type;
+
+import arc.*;
+import arc.audio.*;
+import arc.func.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.scene.ui.*;
+import arc.scene.ui.layout.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.ai.types.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.content.*;
+import mindustry.core.*;
+import mindustry.ctype.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.units.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.meta.*;
+import mindustry.world.meta.values.*;
+
+import static mindustry.Vars.*;
+
+@Override
+public class UnitType extends UnlockableContent{
+    public TextureRegion baseRegion, legRegion, region, shadowRegion, cellRegion,
+        softShadowRegion, jointRegion, footRegion, legBaseRegion, baseJointRegion, ringRegion, ringEndRegion, clawRegion, outlineRegion;
+    public TextureRegion[] wreckRegions;
