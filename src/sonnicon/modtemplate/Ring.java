953e9f77dbe9f08771120c5982ec416e6f409604
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-24 12:17:09 +0100

    Create Ring.java

---
+++ src/sonnicon/modtemplate/Ring.java
@@ -0,0 +1,11 @@
+  
+package mindustry.entities;
+
+import arc.math.geom.*;
+
+public class Ring{
+    public final Vec2 joint = new Vec2(), base = new Vec2();
+    public int group;
+    public boolean moving;
+    public float stage;
+}
