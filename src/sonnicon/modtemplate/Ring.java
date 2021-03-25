914a6bcbee8948bcd7f1dd6e4de24df9fea7e571
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-25 18:58:49 +0100

    Update Ring.java

--- src/sonnicon/modtemplate/Ring.java
+++ src/sonnicon/modtemplate/Ring.java
@@ -4,7 +4,6 @@ package mindustry.entities;
 import arc.math.geom.*;
 
 public class Ring{
-    public final Vec2 joint = new Vec2(), base = new Vec2();
     public int group;
     public boolean moving;
     public float stage;
